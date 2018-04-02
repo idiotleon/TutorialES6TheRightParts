cbb60d0697a87ecc95b06aa110fd9d72fda459d3
Yang Lu <leon@Yangs-MBP.home>
2018-04-01 23:08:38 -0700

    demo example: Promises and This
    Module 1.3

---
+++ ES6TheRightParts/Module1/1_3PromisesAndThis.js
@@ -0,0 +1,72 @@
+p.then(function (v) { return v.id });
+// equivalent
+p.then(v => v.id);
+
+
+p.then(function extractId(v) { return v.id });
+
+
+
+
+var obj = {
+    id: 42,
+    foo: function foo() {
+        setTimeout(function () {
+            console.log(this.id);
+        }, 100);
+    }
+};
+
+obj.foo();  // undefined
+
+var obj = {
+    id: 42,
+    foo: function foo() {
+        setTimeout(function () {
+            console.log(this.id);
+        }.bind(this), 100);
+    }
+};
+
+// equivalent
+// In the arrow function, "this" keyword will automatically use the surronding scope
+var obj = {
+    id: 42,
+    foo: function foo() {
+        setTimeout(() => {
+            // curly braces can be omitted
+            console.log(this.id);
+        }, 100);
+    }
+}
+
+var obj = {
+    id: 42,
+    foo: function foo() {
+        setTimeout(() => {
+
+        });
+    }
+}
+
+// equivalent
+var obj = {
+    id: 42,
+    foo: function foo() {
+        setTimeout(() => {
+            console.log(this.id);
+        }, 100);
+    }
+};
+
+// equivalent
+var obj = {
+    id: 42,
+    foo: function foo() {
+        setTimeout(() =>
+            console.log(this.id)
+            , 100);
+    }
+}
+
+obj.foo();
