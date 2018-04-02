0d22858a4aef4b538d3809445bdcebfb05b76488
Yang Lu <yang.lu3@huawei.com>
2018-04-02 14:44:05 -0700

    demo example: Closures and Explicit Blocks
    Module 2.1

---
+++ ES6TheRightParts/Module2/2_1ClosuresAndExplicitBlocks.js
@@ -0,0 +1,35 @@
+function foo(x, y) {
+    for (let i = 0; i < 10; i++) {
+        $("#btn" + i).click(function () {
+            console.log("button " + i + " clicked!");
+        });
+    }
+}
+
+// equivalent
+function foo(x, y) {
+    for (var i = 0; i < 10; i++) {
+        let j = i;
+        $("#btn" + i).click(function () {
+            console.log("button " + j + " clicked!");
+        });
+    }
+}
+
+function foo(x, y) {
+    // 'let' block form instead of 'let' declaration form
+    let(x = 2) {
+        // ..
+        console.log(x);
+    };
+}
+
+function foo(x, y) {
+    if (x > y) {
+        // explicit block
+        {
+            let x = 2;
+
+        }
+    }
+}
